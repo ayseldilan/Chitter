67d7644e8e6194b7cbef1bee75a7b115185d963a
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-02-25 21:01:43 +0000

    more icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import './Sidebar.css';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Sidebar() {
   return (
@@ -14,7 +19,11 @@ function Sidebar() {
         <SidebarOption Icon={HomeIcon} text="Home" />
         <SidebarOption Icon={SearchIcon} text="Explore" />
         <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-
+        <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+        <SidebarOption Icon={ListAltIcon} text="Lists" />
+        <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+        <SidebarOption Icon={MoreHorizIcon} text="More" />
 
     </div>
   );
