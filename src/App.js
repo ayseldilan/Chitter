a59930d8ee65363f3290b920c17114fd1fdbdbe7
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-02-26 23:07:10 +0000

    adding widgets and completing feed section

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import SidebarOption from './components/SidebarOption';
+import Feed from './components/Feed';
+import Widgets from './components/Widgets';
 
 function App() {
   return (
     <div className="app">
-     <h1> Hi</h1>
-
-     {/* Sidebar goes here */}
      <Sidebar />
      
+     <Feed/>
 
-     {/* Feed stuff */}
+     <Widgets />
 
-     {/* Widgets - what's happening section */}
     </div>
   );
 }
